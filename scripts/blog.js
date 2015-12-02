4d247dd5e4b667fc57289e53bbff4e6936f7177d
Miranda Raymond <mirandaraymond@me.com>
2015-12-02 09:29:21 -0800

    first attempt at tabs, commented out not working yet

--- scripts/blog.js
+++ scripts/blog.js
@@ -63,6 +63,19 @@ $(document).ready(function() {
       }
     }
   })
+  // $('.nav-tabs li a').click(function(event) {
+  //   event.preventDefault();
+  //   var active_tab_selector = $('.nav-tabs li.active a').attr('href');
+  //   $(active_tab_selector).removeClass('active');
+  //   $(active_tab_selector).addClass('hide');
+  //   var actived_nav = $('.nav-tabs li.active');
+  //   actived_nav.removeClass('active');
+  //   $(this).parents('li').addClass('active');
+  //   var target_tab_selector = $(this).attr('href');
+  //   $(target_tab_selector).removeClass('hide');
+  //   $(target_tab_selector).addClass('active');
+  //   )
+  // })
 });
 // option 2
 // $(function () {
