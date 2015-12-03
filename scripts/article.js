69fa81e1cfee8a2bbdaddaabb5f8bdd62ee70f3f
Miranda Raymond <mirandaraymond@me.com>
2015-12-02 22:58:01 -0800

    added html to object costructer

--- scripts/article.js
+++ scripts/article.js
@@ -11,11 +11,11 @@ Article.prototype.toHTML = function() {
   var $clonedArticle = $('article#template').clone();
   $clonedArticle.removeAttr('id');
   $clonedArticle.addClass('blogpost');
-  $clonedArticle.find('h1.author').html('<a href="' + this.authorUrl + '">' + this.author + '</a>');
+  $clonedArticle.find('h1.author').html('Published by: ' + '<a href="' + this.authorUrl + '">' + this.author + '</a>');
   $clonedArticle.find('h2.title').html(this.title);
   $clonedArticle.find('div.post').html(this.body);
   $clonedArticle.find('a.read-more').html("Read More...");
   $clonedArticle.find('h3.publishdate').html(this.publishedOn);
-  $clonedArticle.find('h3.category').html(this.category);
+  $clonedArticle.find('h3.category').html('Category: ' + this.category);
   $('main').append($clonedArticle);
 }
