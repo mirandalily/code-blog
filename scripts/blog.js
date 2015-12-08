f4ad66ae150b850a828d6682d4ea58bbf9d16510
Miranda Raymond <mirandaraymond@me.com>
2015-12-07 17:29:41 -0800

    spent all of today setting up the template in a separate doc and using it for both new and index articles.

--- scripts/blog.js
+++ scripts/blog.js
@@ -11,9 +11,9 @@ blog.sortRawData = function() {
 
 blog.createArticles = function() {
   for (var i = 0; i < blog.rawData.length; i++) {
-    var temp = new Article (blog.rawData[i]);
+    var temp = new Article(blog.rawData[i]);
     blog.articles.push(temp);
-    temp.toHTML();
+    $('.blogart').append(temp.toHTML());
   }
 };
 
@@ -36,66 +36,7 @@ blog.populateCategories = function() {
 };
 
 
-var tabs = function() {
-  $('.articleNav').click(function(event) {
-    event.preventDefault();
-    $('#tab2').hide();
-    $('#tab1').show();
-  });
-  $('.aboutNav').click(function(event) {
-    event.preventDefault();
-    $('#tab1').hide();
-    $('#tab2').show();
-  });
-};
 
-$(document).ready(function() {
-  blog.sortRawData();
-  blog.createArticles();
-  blog.populateAuthors();
-  blog.populateCategories();
-  $('#tab2').hide();
-  tabs();
-
-  $('article p:not(:first-child)').hide();
-  $('a.read-more').on('click', function(event) {
-    event.preventDefault();
-    $(this).parent().find('p').fadeIn();
-    $(this).hide();
-  });
-  $('select#authorfilter').on('change', function(event) {
-    if (event.target.value === 'All') {
-      $('.blogpost').show();
-    } else {
-      $('.blogpost').hide().filter(function() {
-        return $(this).attr('data-author') === event.target.value.replace(/\ /g, '');
-      }).show();
-    }
-  });
-  $('select#categoryfilter').on('change', function(event){
-    if (event.target.value === 'All') {
-      $('.blogpost').show();
-    } else {
-      $('.blogpost').hide().filter(function() {
-        return $(this).attr('data-category') === event.target.value;
-      }).show();
-    }
-  });
-  $( '.cross' ).hide();
-  $( '.hamburger' ).click(function() {
-    $( '.nav' ).slideToggle( 'slow', function() {
-      $( '.hamburger' ).hide();
-      $( '.cross' ).show();
-    });
-  });
-  $( '.cross' ).click(function() {
-    $( '.nav' ).slideToggle( 'slow', function() {
-      $( '.cross' ).hide();
-      $( '.hamburger' ).show();
-    });
-  });
-
-});
 // option 2
 // $(function () {
 //   blog.sortRawData();
--- scripts/new.js
+++ scripts/new.js
@@ -1,60 +1,34 @@
-var PreviewArticle = function(prev) {
-  this.author = prev.author;
-  this.authorSlug = prev.author.replace(/\ /g, '');
-  this.title = prev.title;
-  this.body = prev.body;
-  this.category = prev.category;
-  this.publishedOn = prev.publishedOn;
-  this.authorUrl = prev.authorUrl;
-};
-
-PreviewArticle.prototype.toHTML = function(tagTarget) {
-  var source = $('#previewtemplate').html();
-  var template = Handlebars.compile(source);
-  var result = template(this);
-  $(tagTarget).html(result);
-};
-
-PreviewArticle.prototype.toPlainText = function (textTarget) {
-  var source = $('#textTemplate').html();
-  var template = Handlebars.compile(source);
-  var result = template(this);
-  $(textTarget).html(result);
-};
-
 var getFormData = function () {
-  var grabber = {
+  var grabber = new Article({
     author: $('input[name=author]').val(),
     authorUrl: $('input[name=authorUrl]').val(),
     title: $('input[name=title]').val(),
     category: $('input[name=category]').val(),
     publishedOn: $('input[name=publishedOn]').val(),
-    body: $(marked('textarea[name=body]')).val()
-    //need to add on marked to body somehow
-  };
-  return (grabber);
+    body: marked($('textarea[name=body]').val())
+  });
+  return grabber;
 };
 
-$('button.click').on('click', function(event) {
-  event.preventDefault();
-  var formClick = getFormData();
-  console.log(formClick);
-  var temp2 = new PreviewArticle(formClick);
-  $('pre code').each(function(i, block) {
-    hljs().highlightBlock(block);
+$('document').ready(function() {
+  $.get('template.html', function(data, message, xhr) {
+    Article.prototype.template = Handlebars.compile(data);
+  }).done(function() {
+    $('form').change(function(event) {
+      event.preventDefault();
+      var temp = new Article(getFormData());
+      $('#previewSection').html(temp.toHTML());
+    });
+    $('input[name=preview]').on('click', function(){
+      $('#previewSection').toggle();
+    });
+    $('button.click').on('click', function(event) {
+      event.preventDefault();
+      var temp2 = new Article(getFormData());
+      // $('pre code').each(function(i, block) {
+      //   hljs().highlightBlock(block);
+      // });
+      $('#submitArea').text(temp2.toPlainText());
+    });
   });
-  temp2.toPlainText('#submitArea');
-  // $('#submitArea').html(JSON.stringify(temp2));
-});
-
-$('form').change(function(){
-  event.preventDefault();
-  var formPreview = getFormData();
-  var temp = new PreviewArticle(formPreview);
-  temp.toHTML('#previewSection');
-
-});
-
-$('input[name=preview]').on('click', function(){
-  $('#previewSection').toggle();
 });
